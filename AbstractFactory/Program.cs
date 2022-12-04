8f8992bb9296c5cc30591729a9df00a21361fb42
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-04 01:09:51 -0500

    Improve description

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -3,7 +3,7 @@
  * Type: Creation Pattern
  * Purpose: Provide an interface to create families of related or dependent objects without specifying their concrete classes 
  * Implementation:
- * Abstract Products are created by an Abstract Factory & Client Relies on Abstract Factory (so that multiple factories can be used)
+ * Abstract Products are created by an Abstract Factory & Client Relies on Abstract Factory (so that multiple Concrete Factories can be used)
  * to get Abstract Product implementations (Concrete Products). This way, clients only need to know about the abstractions.
  */
 
