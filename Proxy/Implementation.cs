e05720251b2f615e2e218dbe003e19b4309ff106
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-04 23:55:48 -0500

    Adds: Proxy pattern

---
+++ Proxy/Implementation.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    /// <summary>
+    /// Subject
+    /// </summary>
+    public interface ICelebrity
+    {
+        bool GetApproval();
+    }
+
+    /// <summary>
+    /// RealSubject
+    /// </summary>
+    public class Celebrity : ICelebrity
+    {
+        private string Name;
+
+        public Celebrity(string name)
+        {
+            Name = name;
+        }
+
+        public bool GetApproval()
+        {
+            Console.WriteLine($"Getting Approval from {Name}");
+            Thread.Sleep(1000);
+            return true;
+        }
+
+        public void DisplayCelebrity()
+        {
+            Console.WriteLine($"Name: {Name}");
+        }
+    }
+
+    /// <summary>
+    /// Virtual Proxy
+    /// </summary>
+    public class ManagerProxy : ICelebrity
+    {
+        private Lazy<Celebrity> _Celebrity;
+
+        public ManagerProxy(string celebrityName)
+        {
+            _Celebrity = new Lazy<Celebrity>(() => new Celebrity(celebrityName)); // Thread Safe Lazy Initialization
+        }
+
+        public bool GetApproval()
+        {
+            return _Celebrity.Value.GetApproval();
+        }
+    }
+
+    /// <summary>
+    /// Smart / Protection Proxy
+    /// </summary>
+    public class BodyGuardProxy : ICelebrity
+    {
+        private ManagerProxy _managerProxy;
+        private string _userRole;
+        public BodyGuardProxy(string celebrityName, string userRole)
+        {
+            _userRole = userRole;
+            _managerProxy = new ManagerProxy(celebrityName);
+        }
+
+        public bool GetApproval()
+        {
+            if (_userRole != "knownPerson")
+                throw new UnauthorizedAccessException();
+
+            return _managerProxy.GetApproval();
+        }
+    }
+
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Type: Structural Pattern
+ * Purpose: Creates a surrogate to control access to an object.
+ * Implementation:
+ * Create a wrapper that unifies the use of several other interfaces as members.
+ */
+
+// without proxy
+Console.WriteLine("Constructing Celebrity.");
+var celebrity = new Proxy.Celebrity("Eminem");
+Console.WriteLine($"Celebrity approved?: {celebrity.GetApproval()}.");
+;
+
+Console.WriteLine();
+
+// with proxy 
+Console.WriteLine("Constructing Celebrity Manager proxy.");
+var managerProxy = new Proxy.ManagerProxy("Eminem");
+Console.WriteLine($"Celebrity approved?: {managerProxy.GetApproval()}.");
+
+Console.WriteLine();
+
+// with chained proxy
+Console.WriteLine("Constructing protected BodyGuard proxy.");
+var bodyGuardProxy = new Proxy.BodyGuardProxy("Eminem", "knownPerson");
+Console.WriteLine($"Celebrity approved?: {bodyGuardProxy.GetApproval()}.");
+
+
+Console.WriteLine();
+
+// with chained proxy, no access
+Console.WriteLine("Constructing protected BodyGuard proxy.");
+var bodyGuardProxy2 = new Proxy.BodyGuardProxy("Eminem", "stan");
+Console.WriteLine($"Celebrity approved?: {bodyGuardProxy2.GetApproval()}.");
+
+Console.ReadKey();
