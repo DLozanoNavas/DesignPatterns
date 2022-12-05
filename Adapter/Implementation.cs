8169e7fc864230e4905ae54cbf237cf57b8db780
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-04 21:05:53 -0500

    Adds: Adapter Pattern Implementation

---
+++ Adapter/Implementation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Adapter
+{
+    public class City
+    {
+        public string Name { get; set; }
+        public long HabitantsCount { get; set; }
+    }
+
+    public class Client
+    {
+        ICityAdapter CityAdapter { get;  }
+        public Client(ICityAdapter cityAdapter)
+        {
+            CityAdapter = cityAdapter;
+        }
+       public  void UseCity()
+       {
+           var city = CityAdapter.GetCity();
+           Console.WriteLine(JsonConvert.SerializeObject(city));
+       }
+    }
+
+    public interface ICityAdapter
+    {
+        City GetCity();
+    }
+
+    public class CityAdapter : ICityAdapter
+    {
+        ExternalSystem ExternalSystem { get; set; }
+        public City GetCity()
+        {
+            var cityFromExternalSystem = ExternalSystem.GetCity();
+            return new()
+            {
+                Name = cityFromExternalSystem.Name,
+                HabitantsCount = cityFromExternalSystem.Population,
+            };
+        }
+    }
+
+    public class ExternalSystem
+    {
+        public static CityFromExternalSystem GetCity()
+        {
+            return new()
+            {
+                Name = "Tunja",
+                CountryID = "COL",
+                Population = 200000
+            };
+        }
+    }
+
+    public class CityFromExternalSystem
+    {
+        public string Name { get; set; }
+        public string CountryID { get; set; }
+        public long Population { get; set; }
+    }
+}
---
+++ Adapter/Program.cs
@@ -0,0 +1,8 @@
+﻿using Adapter;
+
+Console.Title = "Adapter Pattern";
+
+var client = new Client(new CityAdapter());
+client.UseCity();
+
+Console.ReadKey();
