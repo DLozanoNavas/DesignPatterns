4789c7a6c18277d2cc6b89b5615b9a57d68ec64e
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-04 22:40:55 -0500

    Adds: Composite Pattern Implementation

---
+++ Composite/Implementation.cs
@@ -0,0 +1,67 @@
+﻿
+
+namespace Composite
+{
+    public interface IFileSystemItem
+    {
+        string Name { get; set; }
+        long GetSize();
+    }
+
+    public abstract class FileSystemItem : IFileSystemItem
+    {
+        public string Name { get; set; }
+
+        protected FileSystemItem(string name)
+        {
+            Name = name;
+        }
+
+        public abstract long GetSize();
+    }
+
+    public class Folder : FileSystemItem
+    {
+        private readonly List<FileSystemItem> _fileSystemItems = new();
+        public Folder(string name) : base(name)
+        {
+        }
+
+        public void Remove(FileSystemItem file)
+        {
+            _fileSystemItems.Remove(file);
+        }
+        
+        public void Add(FileSystemItem file)
+        {
+            _fileSystemItems.Add(file);
+        }
+
+        public override long GetSize()
+        {
+            return GetSize(_fileSystemItems);
+        }
+
+        long GetSize(List<FileSystemItem> fileSystemItems)
+        {
+            return fileSystemItems.Sum(c => c.GetSize());
+        }
+
+
+    }
+
+    public class File :  FileSystemItem
+    {
+        private long _size { get; }
+        public File(string name, long size) : base(name)
+        {
+            _size = size;
+        }
+
+        public override long GetSize()
+        {
+            return _size;
+        }
+    }
+
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Type: Structural Pattern
+ * Purpose: Compose objects intro tree structures to represent part-whole hierarchies.
+ * Implementation:
+ * Use object composition to allow for recursive operations on the hierarchical tree.
+ */
+
+using Composite;
+
+Console.Title = "Composite Pattern";
+
+var root = new Composite.Folder("root");
+var topLevelFile = new Composite.File("notes.txt",10);
+var topLevelFolder1 = new Folder("topLevelFolder1");
+var topLevelFolder2 = new Folder("topLevelFolder2");
+
+root.Add(topLevelFile);
+root.Add(topLevelFolder1);
+root.Add(topLevelFolder2);
+
+var subLevelFile1 = new Composite.File("doc.pdf", 200);
+var subLevelFile2 = new Composite.File("pic.png", 150);
+
+topLevelFolder2.Add(subLevelFile1);
+topLevelFolder2.Add(subLevelFile2);
+
+Console.WriteLine($"Size of topLevelDirectory1: {topLevelFolder1.GetSize()}");
+Console.WriteLine($"Size of topLevelDirectory2: {topLevelFolder2.GetSize()}");
+Console.WriteLine($"Size of root folder: {root.GetSize()}");
+
+Console.ReadKey();
