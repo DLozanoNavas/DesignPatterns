dfdca1387dea7161916f9c85227b6fb93cf617a7
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-04 21:49:38 -0500

    Adds: Bridge Pattern

---
+++ Bridge/Implementation.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    /// <summary>
+    /// Abstraction
+    /// </summary>
+    public abstract class Cart
+    {
+        protected readonly ICoupon _coupon;
+        protected double Amount { get; }
+        protected double Total { get; set; }
+
+        protected Cart(ICoupon coupon)
+        {
+            _coupon = coupon;
+            Amount = 200;
+        }
+
+        public abstract void CalculateTotal();
+    }
+
+    /// <summary>
+    /// Implementor
+    /// </summary>
+    public interface ICoupon
+    {
+        double CouponValue { get; }
+    }
+
+    /// <summary>
+    /// Redefined Abstraction
+    /// </summary>
+    public class SpainCart : Cart
+    {
+        public SpainCart(ICoupon coupon) : base(coupon)
+        {
+        }
+        public override void CalculateTotal()
+        {
+            Total = Amount - (Amount * (_coupon.CouponValue / 100));
+
+            Console.WriteLine($"SPAIN CART");
+            Console.WriteLine($"Sub Total: ${Amount}");
+            Console.WriteLine($"Discount: {(_coupon.CouponValue)}%");
+            Console.WriteLine($"Total: ${Total}");
+        }
+    }
+    /// <summary>
+    /// Redefined Abstraction
+    /// </summary>
+    public class ItalyCart : Cart
+    {
+        public ItalyCart(ICoupon coupon) : base(coupon)
+        {
+        }
+        public override void CalculateTotal()
+        {
+            Total = Amount - (Amount * (_coupon.CouponValue / 100));
+            Console.WriteLine($"ITALY CART");
+            Console.WriteLine($"Sub Total: ${Amount}");
+            Console.WriteLine($"Discount: {(_coupon.CouponValue)}%");
+            Console.WriteLine($"Total: ${Total}");
+        }
+    }
+    /// <summary>
+    /// Concrete Implementor
+    /// </summary>
+    public class NoCoupon : ICoupon
+    {
+        public double CouponValue => 0;
+    }
+    /// <summary>
+    /// Concrete Implementor
+    /// </summary>
+    public class FivePercentCoupon : ICoupon
+    {
+        public double CouponValue => 5;
+    }
+    /// <summary>
+    /// Concrete Implementor
+    /// </summary>
+    public class TenPercentCoupon : ICoupon
+    {
+        public double CouponValue => 10;
+    }
+
+
+}
---
+++ Bridge/Program.cs
@@ -0,0 +1,16 @@
+﻿using Bridge;
+
+Console.Title = "Bridge Pattern";
+
+/*
+ * Type: Structural Pattern
+ * Purpose: Decouple an abstraction from its implementation so that the two can vary independently.
+ * Implementation:
+ * Abstraction defines the abstraction´s interface and holds a reference to the Implementor.
+ */
+
+var cart = new ItalyCart(new TenPercentCoupon());
+
+cart.CalculateTotal();
+
+Console.ReadKey();
