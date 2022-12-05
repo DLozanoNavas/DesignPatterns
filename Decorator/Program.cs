f09f0cfba1c1260c30ca6d756ce931bc28781c3f
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-04 22:07:29 -0500

    Update Dercription

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -1,6 +1,14 @@
-﻿
+﻿using Decorator;
 
-using Decorator;
+Console.Title = "Decorator Pattern";
+
+/*
+ * Type: Structural Pattern
+ * Purpose: Attach additional responsibilities to an object (not to a class).
+ * Implementation:
+ * Use wrappers to add new methods and properties.
+ */
+
 
 var ps = new PaymentsServiceDecoratorBase(new StripePaymentsService()); // Can be switched to BraintreePaymentsService
 ps.ProcessPayment("1234 4567 8901 1234", "12/27", "123");
