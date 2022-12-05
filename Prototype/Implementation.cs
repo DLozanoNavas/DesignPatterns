781173513eb84e4ec852ca5f7c13ab520f35af31
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-04 20:52:27 -0500

    Adds: Prototype Pattern Code

---
+++ Prototype/Implementation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Prototype
+{
+
+    public abstract class Person
+    {
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+        public abstract Person ShallowClone();
+        public abstract Person? DeepClone();
+    }
+
+    public class Manager: Person
+    {
+        public Manager(string name) : base(name)
+        {
+            
+        }
+
+        public override Person ShallowClone()
+        {
+            return (Person)MemberwiseClone();
+        }
+        public override Person? DeepClone()
+        {
+            return JsonConvert.DeserializeObject<Manager>(JsonConvert.SerializeObject(this));
+        }
+    }
+
+    public class Employee: Person
+    {
+        public Manager Manager { get; set; }
+        public Employee(string name, Manager manager) : base(name)
+        {
+            Manager = manager;
+        }
+        public override Person ShallowClone()
+        {
+            return (Person)MemberwiseClone();
+        }
+        
+        public override Person? DeepClone()
+        {
+            return JsonConvert.DeserializeObject<Employee>(JsonConvert.SerializeObject(this));
+        }
+    }
+
+
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Prototype;
+
+Console.Title = "Prototype Pattern";
+
+var manager = new Manager("Anna");
+var managerClone = (Manager)manager.ShallowClone();
+Console.WriteLine($"Manager was cloned: {managerClone.Name}");
+
+
+var employee = new Employee("John", managerClone);
+var employeeClone = (Employee)employee.DeepClone()!;
+Console.WriteLine($"Employee was cloned: {employeeClone.Name} with manager {employeeClone.Manager.Name}");
+
+
+Console.WriteLine($"{employeeClone.Name}'s Manager is {employeeClone.Manager.Name}");
+managerClone.Name = "Carlos";
+Console.WriteLine($"{employeeClone.Name}'s Manager is {employeeClone.Manager.Name}");
+
+Console.ReadKey();
