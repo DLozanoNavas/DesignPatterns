b48f70d5ef69134d4c1337eec41f99db5c46a502
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-04 22:04:50 -0500

    Adds: Decorator Implementation

---
+++ Decorator/Implementation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    /// <summary>
+    /// Component 
+    /// </summary>
+    public interface IPaymentsService
+    {
+        bool ProcessPayment(string creditCardNumber, string crefirCardExpiration, string CVC);
+    }
+    /// <summary>
+    /// Concrete Component
+    /// </summary>
+    public class StripePaymentsService : IPaymentsService
+    {
+        public bool ProcessPayment(string creditCardNumber, string crefirCardExpiration, string CVC)
+        {
+            Console.WriteLine("Processing Payment through Stripe");
+            return true;
+        }
+    }
+    /// <summary>
+    /// Concrete Component
+    /// </summary>
+    public class BraintreePaymentsService : IPaymentsService
+    {
+        public bool ProcessPayment(string creditCardNumber, string crefirCardExpiration, string CVC)
+        {
+            Console.WriteLine("Processing Payment through Braintree");
+            return true;
+        }
+    }
+    /// <summary>
+    /// Decorator
+    /// </summary>
+    public class PaymentsServiceDecoratorBase : IPaymentsService
+    {
+        private IPaymentsService PaymentsService { get; set; }
+
+        public PaymentsServiceDecoratorBase(IPaymentsService paymentsService)
+        {
+            PaymentsService = paymentsService;
+        }
+
+        public virtual bool ProcessPayment(string creditCardNumber, string crefirCardExpiration, string CVC)
+        {
+            return PaymentsService.ProcessPayment(creditCardNumber, crefirCardExpiration, CVC);
+        }
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+using Decorator;
+
+var ps = new PaymentsServiceDecoratorBase(new StripePaymentsService()); // Can be switched to BraintreePaymentsService
+ps.ProcessPayment("1234 4567 8901 1234", "12/27", "123");
+
+
+Console.ReadKey();
