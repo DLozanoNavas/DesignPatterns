f7cb55096c91c5972b95b933b26ee895d4d41b75
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-04 23:06:25 -0500

    Adds: Facade Implementation

---
+++ Facade/Implementation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    /// <summary>
+    /// Subsystem class
+    /// </summary>
+    public class Cart
+    {
+        public Cart(double total)
+        {
+            Total = total;
+        }
+        public double Total { get; set; }
+
+    }
+
+    /// <summary>
+    /// Subsystem class
+    /// </summary>
+    public class CustomerDiscountBaseService
+    {
+        public double GetDiscountPercentage(Cart cart)
+        {
+            return (cart.Total > 10) ? 10 : 0;
+        }
+    }
+
+    /// <summary>
+    /// Subsystem class
+    /// </summary>
+    public class DayOfTheWeekDiscountService
+    {
+        public double CalculateDayOfTheWeekDiscount()
+        {
+            // fake calculation for demo purposes
+            return DateTime.UtcNow.DayOfWeek switch
+            {
+                DayOfWeek.Saturday => 10,
+                DayOfWeek.Sunday => 10,
+                DayOfWeek.Monday => 1,
+                DayOfWeek.Tuesday => 2,
+                DayOfWeek.Wednesday => 3,
+                DayOfWeek.Thursday => 4,
+                DayOfWeek.Friday => 5,
+                _ => 0
+            };
+        }
+    }
+
+    /// <summary>
+    /// Facade
+    /// </summary>
+    public class CartsServiceFacade
+    {
+        private readonly CustomerDiscountBaseService _customerDiscountBaseService = new();
+        private readonly DayOfTheWeekDiscountService _dayOfTheWeekDiscountService = new();
+
+        public double CalculateDiscountPercentage(Cart cart)
+        {
+            return _customerDiscountBaseService.GetDiscountPercentage(cart) + _dayOfTheWeekDiscountService.CalculateDayOfTheWeekDiscount();
+        }
+
+    }
+}
---
+++ Facade/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Runtime.Serialization;
+using Facade;
+
+Console.Title = "Facade Pattern";
+
+/*
+ * Type: Structural Pattern
+ * Purpose: Group a set of system interfaces into a unified interface wrapper to provide an abstraction that's easier to use.
+ * Implementation:
+ * Create a wrapper that unifies the use of several other interfaces as members.
+ */
+
+var cart = new Cart(15);
+
+var facade = new CartsServiceFacade();
+Console.WriteLine($"Cart Discount is: {facade.CalculateDiscountPercentage(cart)}%");
+
+Console.ReadKey();
